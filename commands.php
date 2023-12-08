6f21d5136def1e748c971e7cd0b4a1bfdac114de
Buzz Moody <buzz+github@buzz.id.au>
2023-12-09 09:41:48 +1100

    Update commands.php

--- commands.php
+++ commands.php
@@ -388,11 +388,12 @@ class Commands {
 	
 	function radar($message, $discord) {
 		
+		file_put_contents("radar.gif", file_get_contents("http://www.bom.gov.au/radar/IDR023.gif"));
 		$embed = $discord->factory(Embed::class);
 		$embed->setTitle("Melbourne Weather Radar")
 			->setURL("http://www.bom.gov.au/products/IDR023.loop.shtml")
 			->setDescription($this->getTemp())
-			->setImage("http://www.bom.gov.au/radar/IDR023.gif")
+			->setImage("/home/buzz/bot-php/radar.gif");
 			->setColor("0x00A9FF")
 			->setTimestamp()
 			->setFooter("BOM", "http://www.bom.gov.au/images/touch-icon/touch-icon-76x76.png");
