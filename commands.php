583dbfbff8d700dfe6e0ca56ba680e9cf3d6290f
Buzz Moody <buzz+github@buzz.id.au>
2024-06-30 13:35:13 +1000

    Update commands.php

--- commands.php
+++ commands.php
@@ -82,7 +82,7 @@ class Commands {
 		$getUD = (empty($args)) ? @file_get_contents("https://www.urbandictionary.com/random.php") : @file_get_contents("https://www.urbandictionary.com/define.php?term=".urlencode($args));
 		if (empty($getUD)) { return $message->channel->sendMessage("Word not found"); }
 		preg_match_all("/(:?href=\"\/define\.php\?term=(.+)\" id=\"\d+\">(.+)<\/a><\/h1>|<div class=\"break-words meaning mb-4\">(.+)<\/div>)/mU", $getUD, $matches);
-		$message->channel->sendMessage("**".str_replace('+',' ', $matches[2][0])."**: ".strip_tags($matches[4][1]));
+		$message->channel->sendMessage("**".str_replace('+',' ', $matches[2][0])."**: ".html_entity_decode(strip_tags($matches[4][1])));
 	}
 	
 	function dice($message, $discord, $args) {
