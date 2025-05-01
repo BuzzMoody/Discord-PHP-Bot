96dc185f2dcd45a6b5ed6cf4f140085721b1d64a
Buzz Moody <buzz+github@buzz.id.au>
2025-05-01 17:26:50 +1000

    Update Utils.php

--- Commands/Utils.php
+++ Commands/Utils.php
@@ -12,7 +12,7 @@
 	
 		if (empty($args)) { return $message->reply("Maybe give me something to search for??"); }
 		
-		$api_url = ($type == 'google') ? "https://www.googleapis.com/customsearch/v1?key={$keys['google']}&cx=017877399714631144452:hlos9qn_wvc&googlehost=google.com.au&num=1&q=".str_replace(' ', '%20', $args) : "https://www.googleapis.com/customsearch/v1?key={$keys['google']}&cx=017877399714631144452:0j02gfgipjq&googlehost=google.com.au&searchType=image&excludeTerms=youtube&imgSize=xxlarge&safe=off&num=1&fileType=jpg,png,gif&q=".str_replace(' ', '%20', $args)."%20-site:facebook.com%20-site:tiktok.com%20-site:instagram.com";
+		$api_url = ($type == 'google') ? "https://customsearch.googleapis.com/customsearch/v1?key={$keys['google']}&cx=017877399714631144452:hlos9qn_wvc&googlehost=google.com.au&num=1&q=".str_replace(' ', '%20', $args) : "https://customsearch.googleapis.com/customsearch/v1?key={$keys['google']}&cx=017877399714631144452:0j02gfgipjq&googlehost=google.com.au&searchType=image&excludeTerms=youtube&imgSize=xxlarge&safe=off&num=1&fileType=jpg,png,gif&q=".str_replace(' ', '%20', $args)."%20-site:facebook.com%20-site:tiktok.com%20-site:instagram.com";
 		
 		try {
 			$search = file_get_contents($api_url);
