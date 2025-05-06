52d405135d116d3ffade41998f0ec99fefa6b99f
Buzz Moody <buzz+github@buzz.id.au>
2025-05-06 21:25:25 +1000

    Update StableDiffusion.php

--- Commands/StableDiffusion.php
+++ Commands/StableDiffusion.php
@@ -3,24 +3,29 @@
 	use React\Http\Browser;
 	use Psr\Http\Message\ResponseInterface;
 	use Discord\Parts\Channel\Attachment;
+	use React\Socket\Connector
 	use Discord\Builders\MessageBuilder;
 
 	function StableDiffusion($message, $args) { 
 	
-		global $keys;
-	
-		$p = $keys['cloud'];
-		$l = "australia-southeast1";
-		$m = "imagen-3.0-generate-002";
-		$e = "australia-southeast1-aiplatform.googleapis.com";
-		$s = "../Media/AI";
+		global $keys
 		
 		$prompt = $args;
-
-		$t = trim(shell_exec('gcloud auth print-access-token 2>&1'));
-
-		$u = "https://$e/v1/projects/$p/locations/$l/publishers/google/models/$m:predict";
-		$d = [
+		$model = "imagen-3.0-generate-002";
+		$gcloud = trim(shell_exec('gcloud auth print-access-token 2>&1'));
+		$url = "https://australia-southeast1-aiplatform.googleapis.com/v1/projects/{$keys['cloud']}/locations/australia-southeast1/publishers/google/models/$model:predict";
+		
+		$client->get("{$keys['sd']}/?img={$args}")->then(function (ResponseInterface $response) use ($message) {
+			$rand = rand(1,100000);
+			file_put_contents("../Media/AI/{$rand}.png", $response->getBody());
+			$builder = MessageBuilder::new()
+				->addFile("../Media/AI/{$rand}.png", "{$rand}.png");
+			return $message->channel->sendMessage($builder);
+		}, function (Exception $e) {
+			echo "Error: {$e->getMessage()}\n";
+		});
+		
+		$postData = [
 			"instances" => [["prompt" => $prompt]],
 			"parameters" => [
 				"aspectRatio" => "16:9", "sampleCount" => 1, "negativePrompt" => "",
@@ -28,21 +33,17 @@
 				"addWatermark" => true, "includeRaiReason" => true, "language" => "auto",
 			]
 		];
-		$j = json_encode($d);
-
-		$c = curl_init();
-		curl_setopt_array($c, [
-			CURLOPT_URL => $u, CURLOPT_RETURNTRANSFER => true, CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => $j, CURLOPT_HTTPHEADER => [
-				'Content-Type: application/json', 'Authorization: Bearer ' . $t,
-				'Content-Length: ' . strlen($j)
-			]
-		]);
-		$r = curl_exec($c);
-		$h = curl_getinfo($c, CURLINFO_HTTP_CODE);
-		curl_close($c);
-
-		$rd = json_decode($r, true);
+		$postDataEnc = json_encode($postData);
+		$headers = [
+			'Content-Type: application/json', 'Authorization: Bearer ' . $gcloud,
+			'Content-Length: ' . strlen($postDataEnc)
+			
+		$browser = new Browser();
+		$browser->post($url, $headers, $postDataEnc)->then(function (ResponseInterface $response) {
+			$responseBody = (string) $response->getBody();
+			$responseData = json_decode($responseBody);
+			print_r($responseData);
+		});
 		
 		$b64 = $rd['predictions'][0]['bytesBase64Encoded'];
 		$mt = $rd['predictions'][0]['mimeType'];
@@ -52,10 +53,10 @@
 		$f = 'img_' . time() . '_' . uniqid() . '.' . $ext;
 		$fp = rtrim($s, '/') . '/' . $f;
 		
-		file_put_contents($fp, $bin);
+		// file_put_contents($fp, $bin);
 		
-		$message->channel->sendMessage(MessageBuilder::new()
-			->addFile($fp));
+		//$message->channel->sendMessage(MessageBuilder::new()
+		//	->addFile($fp));
 
 
 	}
