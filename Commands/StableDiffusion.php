bed84c649e0991fb6bbdf2e6f1b27ac6f063feaf
Buzz Moody <buzz+github@buzz.id.au>
2025-05-12 17:57:21 +1000

    Update StableDiffusion.php

--- Commands/StableDiffusion.php
+++ Commands/StableDiffusion.php
@@ -28,6 +28,7 @@
 			function (ResponseInterface $response) use ($message) {
 				$responseBody = $response->getBody();
 				$responseData = json_decode($responseBody);
+				print_r($responseData);
 				$base64 = $responseData['candidates'][0]['content']['parts'][1]['inlineData']['data'];
 				$mimeType = $responseData['candidates'][0]['content']['parts'][1]['inlineData']['mimeType'];
 				echo "Mime: ".$mimeType."\n";
