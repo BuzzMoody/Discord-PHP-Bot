f94f18254a9b26942110fda8049e1282ad434c07
Buzz Moody <buzz+github@buzz.id.au>
2025-05-12 18:06:43 +1000

    Update StableDiffusion.php

--- Commands/StableDiffusion.php
+++ Commands/StableDiffusion.php
@@ -36,8 +36,8 @@
 				$ext = preg_replace('/[^a-z0-9]/i', '', str_replace('image/', '', $mimeType)) ?: 'png';
 				$filename = 'image_' . time() . '_' . uniqid() . '.' . $ext;
                 $filePath = "../Media/AI/" . $filename;
-				//file_put_contents($filePath, $bin);
-				$builder = MessageBuilder::new()->addFileFromContent('image.png', $bin);
+				file_put_contents($filePath, $bin);
+				$builder = MessageBuilder::new()->addFile($filePath, $filename);
 				$message->channel->sendMessage($builder);
 			},
 			function (Exception $e) {
