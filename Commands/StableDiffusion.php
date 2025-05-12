e992c57c301cf84f9f1aef271634174bf66f84da
Buzz Moody <buzz+github@buzz.id.au>
2025-05-12 17:49:51 +1000

    Update StableDiffusion.php

--- Commands/StableDiffusion.php
+++ Commands/StableDiffusion.php
@@ -10,17 +10,12 @@
 		global $keys;
 		
 		$prompt = $args;
-		$apicode = $keys['cloud'];
-		$model = "imagen-3.0-generate-002";
-		$gcloud = trim(shell_exec('gcloud auth print-access-token 2>&1'));		
-		$url = "https://australia-southeast1-aiplatform.googleapis.com/v1/projects/$apicode/locations/australia-southeast1/publishers/google/models/$model:predict";	
+		$url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation:generateContent?key=".$keys['gemini'];	
 		
 		$postData = [
-			"instances" => [["prompt" => $prompt]],
-			"parameters" => [
-				"aspectRatio" => "16:9", "sampleCount" => 1, "negativePrompt" => "",
-				"enhancePrompt" => false, "personGeneration" => "", "safetySetting" => "",
-				"addWatermark" => true, "includeRaiReason" => true, "language" => "auto",
+			"contents" => [["parts" => [["text" => $prompt]]]],
+			"generationConfig" => [
+				"responseModalities" => ["TEXT", "IMAGE"]
 			]
 		];
 		$postDataEnc = json_encode($postData);
@@ -35,16 +30,7 @@
 			function (ResponseInterface $response) use ($message) {
 				$responseBody = $response->getBody();
 				$responseData = json_decode($responseBody);
-				$base64 = $responseData['predictions'][0]['bytesBase64Encoded'];
-				$mimeType = $responseData['predictions'][0]['mimeType'];
-				$bin = base64_decode($base64);
-				$ext = preg_replace('/[^a-z0-9]/i', '', str_replace('image/', '', $mimeType)) ?: 'png';
-				
-				$filename = 'image_' . time() . '_' . uniqid() . '.' . $ext;
-                $filePath = "../Media/AI/" . $filename;
-                
-				file_put_contents($filePath, $bin);
-				
+				file_put_contents("vertex.log", print_r($responseData, true));				
 			},
 			function (Exception $e) {
 				echo "Error: ".$e->getMessage();
