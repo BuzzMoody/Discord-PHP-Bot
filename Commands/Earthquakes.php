8130591486eb2fa641f123e26887f6773fa93007
Buzz Moody <buzz+github@buzz.id.au>
2025-08-17 20:01:27 +1000

    Update Earthquakes.php

--- Commands/Earthquakes.php
+++ Commands/Earthquakes.php
@@ -11,18 +11,15 @@
 		global $discord;
 		
 		$guild = $discord->guilds->get('id', '232691831090053120');
-		$channel = $guild->channels->get('id', '232691831090053120');
+		$channel = $guild->channels->get('id', '274828566909157377');
 		
 		$currentTime = new DateTime('now', new DateTimeZone('UTC'));
-		$yesterdayTime = clone $currentTime;
-		$yesterdayTime->sub(new DateInterval('P1D'));
+		$priorTime = clone $currentTime;
+		$priorTime->sub(new DateInterval('P2D'));
 		$currentFormatted = $currentTime->format('Y-m-d\TH:i:s\Z');
-		$yesterdayFormatted = $yesterdayTime->format('Y-m-d\TH:i:s\Z');
+		$priorFormatted = $priorTime->format('Y-m-d\TH:i:s\Z');
 		
-		$embed = $discord->factory(Embed::class);
-		$embed->setTitle("⚠️ Earthquake Alert ⚠️");
-		
-		$url = "https://ui.earthquakes.ga.gov.au/geoserver/earthquakes/wfs?service=WFS&request=getfeature&typeNames=earthquakes:earthquakes&outputFormat=application/json&CQL_FILTER=display_flag=%27Y%27%20AND%20origin_time%20BETWEEN%20{$yesterdayFormatted}%20AND%20{$currentFormatted}%20AND%20located_in_australia=%27Y%27&sortBy=origin_time%20D";
+		$url = "https://ui.earthquakes.ga.gov.au/geoserver/earthquakes/wfs?service=WFS&request=getfeature&typeNames=earthquakes:earthquakes&outputFormat=application/json&CQL_FILTER=display_flag=%27Y%27%20AND%20origin_time%20BETWEEN%20{$priorFormatted}%20AND%20{$currentFormatted}%20AND%20located_in_australia=%27Y%27&sortBy=origin_time%20D";
 		
 		$headers = [
 			'accept' => '*/*',
@@ -43,15 +40,31 @@
 		
 		$client = new Browser();
 		$client->get($url, $headers)->then(
-			function (ResponseInterface $response) use ($embed) {
+			function (ResponseInterface $response) use ($channel, $discord) {
 				
 				$responseBody = $response->getBody();
 				$responseData = json_decode($responseBody);
 				
-				print_r($responseData);
+				if ($responseData->totalFeatures >= 1) {
+					
+					foreach ($responseData->features as $quakes) {
+						
+						$embed = $discord->factory(Embed::class);
+						$embed->setTitle("⚠️ Earthquake Alert ⚠️")
+							->setDescription("Magnitude **{$quakes->properties->preferred_magnitude}** earthquake detected at a depth of **{$quakes->properties->depth} km**\n\nLocation: **{$quakes->properties->description}**")
+							->setColor(getenv('COLOUR'))
+							->setURL("https://earthquakes.ga.gov.au/event/{$quakes->properties->event_id}")
+							->setFooter("Geoscience Australia");
+						
+						$channel->sendEmbed($embed);
+						unset($embed);
+						
+					}
+					
+				}
 				
 			},
-			function (Exception $e) use ($message) {
+			function (Exception $e) use ($channel) {
 				$channel->sendMessage('Error: ' . $e->getMessage());
 			}
 		);
