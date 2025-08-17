da8c7875931afdcae5818e5bb982e7c5ed193835
Buzz Moody <buzz+github@buzz.id.au>
2025-08-17 17:47:50 +1000

    Update Earthquakes.php

--- Commands/Earthquakes.php
+++ Commands/Earthquakes.php
@@ -41,6 +41,7 @@
 			'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
 		];
 		
+		$client = new Browser();
 		$client->get($url, $headers)->then(
 			function (ResponseInterface $response) use ($embed) {
 				
