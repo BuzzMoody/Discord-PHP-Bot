d82027cba29c22de755423a6ce0d19f880a86c4a
Buzz Moody <buzz+github@buzz.id.au>
2025-08-31 14:35:21 +1000

    Refactor Apex command to use async HTTP and embeds
    Replaces synchronous file_get_contents with ReactPHP's async HTTP client for fetching Apex Legends map status. Adds rich embed formatting for Discord messages, improving user experience and error handling.

--- Commands/Apex.php
+++ Commands/Apex.php
@@ -1,12 +1,38 @@
 <?php
 
+	use Discord\Parts\Embed\Embed;
+	use Discord\Parts\Channel\Attachment;
+	use Discord\Builders\MessageBuilder;
+	use React\Http\Browser;
+	use Psr\Http\Message\ResponseInterface;
+
 	function Apex($message) {
 		
-		$get = file_get_contents("https://apexlegendsstatus.com/current-map/battle_royale/pubs");
-		preg_match('/<h3 .*>(.+)<\/h3>.+ ends in (.+)<\/p>/U', $get, $data);
-		preg_match_all('/<h3 .*>(.+)<\/h3>/U', $get, $next);
+		// $get = file_get_contents("https://apexlegendsstatus.com/current-map/battle_royale/pubs");
+		// preg_match('/<h3 .*>(.+)<\/h3>.+ ends in (.+)<\/p>/U', $get, $data);
+		// preg_match_all('/<h3 .*>(.+)<\/h3>/U', $get, $next);
 	
-		$message->channel->sendMessage($data[1]." ends in ".$data[2]." | Next Map: ".$next[1][1]);
+		// $message->channel->sendMessage($data[1]." ends in ".$data[2]." | Next Map: ".$next[1][1]);
+		
+		global $discord;
+		
+		$http = new Browser();
+		$http->get('https://apexlegendsstatus.com/current-map/battle_royale/pubs')->then(
+			function (ResponseInterface $response) use ($message, $discord) {
+				$output = $response->getBody();
+				preg_match('/<h3 .*>(.+)<\/h3>.+ ends in (.+)<\/p>/U', $output, $data);
+				preg_match_all('/<h3 .*>(.+)<\/h3>/U', $output, $next);
+				$embed = $discord->factory(Embed::class);
+				$embed->setAuthor('Apex Legends Status', 'https://apexlegendsstatus.com/assets/layout/favicon-dark-scheme-32x32.png', 'https://apexlegendsstatus.com/')
+					->setColor(getenv('COLOUR'))
+					->setDescription("The current map is **{$data[1]}**.\n\nThe next map is **{$next[1][1]}** and starts in **{$data[2]}** minutes")
+					->setImage("https://apexlegendsstatus.com/assets/maps/".str_replace(' ', '_', $data[1]).".png");
+				$message->channel->sendEmbed($embed);
+			},
+			function (Exception $e) use ($message) {
+				$message->channel->send("Error: {$e->getMessage()}");
+			}
+		);
 		
 	}
 	
