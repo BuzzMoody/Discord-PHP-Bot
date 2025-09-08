232c5f03ec266f42e73d1256053a7851ca1a1f74
Buzz Moody <buzz+github@buzz.id.au>
2025-09-08 12:23:09 +1000

    Update Test.php

--- Commands/Test.php
+++ Commands/Test.php
@@ -8,10 +8,10 @@
 			
 				$guild = $discord->guilds->get('id', '232691831090053120');
 				$channel = $guild->channels->get('id', '274828566909157377');
-				$message2 = $channel->messages->fetch('1414434587677032499');
-				print_r($message2);
-				return $message2->reply("Replying to message");
-				
+				$message2 = $channel->messages->fetch('1414434587677032499')->then(function ($message2) {
+					print_r($message2);
+					return $message2->reply("Replying to message");
+				});				
 		
 		}
 		
