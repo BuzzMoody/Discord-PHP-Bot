24aa4be0520aebab07da708aff7c9a3b129f2838
Buzz Moody <buzz+github@buzz.id.au>
2025-09-08 12:25:02 +1000

    Update Test.php

--- Commands/Test.php
+++ Commands/Test.php
@@ -6,13 +6,12 @@
 	
 		if (isAdmin($message->author->id) && getenv('BETA') === 'true') {
 			
-				$guild = $discord->guilds->get('id', '232691831090053120');
-				$channel = $guild->channels->get('id', '274828566909157377');
-				$message2 = $channel->messages->fetch('1414434587677032499')->then(function ($message2) {
-					print_r($message2);
-					return $message2->reply("Replying to message");
-				});				
-		
+			$guild = $discord->guilds->get('id', '232691831090053120');
+			$channel = $guild->channels->get('id', '274828566909157377');
+			$message2 = $channel->messages->fetch('1414434587677032499')->then(function ($message2) {
+				return simpleEmbed("Chat Reminders", "attachment://bot.webp", "This is your reminder or some shit I dunno", $message2, true, null); 
+			});				
+
 		}
 		
 	}
