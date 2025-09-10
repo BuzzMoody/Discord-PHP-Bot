8f46ffb93dc5f75e37ef84e99cebcf11ed0b952f
Buzz Moody <buzz+github@buzz.id.au>
2025-09-11 07:07:47 +1000

    Update ListReminders.php

--- Commands/ListReminders.php
+++ Commands/ListReminders.php
@@ -30,10 +30,9 @@
 
 				foreach ($fetchedMessages as $msg) {
 					$timestamp = strtotime($msg->timestamp);
-					$dateTime = new DateTime("@$timestamp", new DateTimeZone('Australia/Melbourne'));
-					$offset = $dateTime->getOffset();
-					$timestamp = $timestamp + $offset;
-					$embed->addFieldValues("Created <t:{$timestamp}:R> in https://discord.com/channels/232691831090053120/{$msg->channel_id}/{$msg->id}", "*{$msg->content}*", false);
+					$dateTime = new DateTime("@$timestamp");
+					$dateTime->setTimezone(new DateTimeZone('Australia/Melbourne'));
+					$embed->addFieldValues("Created <t:{$dateTime->getTimestamp()}:R> in https://discord.com/channels/232691831090053120/{$msg->channel_id}/{$msg->id}", "*{$msg->content}*", false);
 				}
 				$builder = MessageBuilder::new()
 					->addEmbed($embed)
