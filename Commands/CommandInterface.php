3917fc58526fc56eefb3e42b2c74d1a79f458058
Buzz Moody <buzz+github@buzz.id.au>
2025-10-02 20:35:35 +1000

    Update execute method signature in command classes
    Standardized the execute method signature in AbstractCommand and CommandInterface by removing default null values and renaming the second parameter in AbstractCommand. This ensures consistency across command implementations.

--- Commands/CommandInterface.php
+++ Commands/CommandInterface.php
@@ -5,7 +5,7 @@ interface CommandInterface {
 	public function getName(): string;
 	public function getDesc(): string;
 	public function getPattern(): string;
-	public function execute($message, $args = null, $matches = null);
+	public function execute($message, $args, $matches);
 
 }
 
