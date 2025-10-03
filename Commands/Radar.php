3fd6b28380ce2ee3eccee89895ed10383378653d
Buzz Moody <buzz+github@buzz.id.au>
2025-10-03 18:33:06 +1000

    Refactor Radar command to use class-based structure
    Removed legacy Forecast and Radar command scripts and introduced a new class-based Radar command in Commands/Radar.php. The new implementation extends AbstractCommand, improving maintainability and consistency with other commands.

---
+++ Commands/Radar.php
@@ -0,0 +1,37 @@
+<?php
+
+	use Discord\Parts\Embed\Embed;
+	use Discord\Parts\Channel\Attachment;
+	use Discord\Builders\MessageBuilder;
+	
+	class Radar extends AbstractCommand {
+		
+		public function getName(): string {
+			return 'Radar';
+		}
+		
+		public function getDesc(): string {
+			return 'Current weather radar imagery for Melbourne.';
+		}
+		
+		public function getPattern(): string {
+			return '/^radar$/';
+		}
+		
+		public function execute($message, $args, $matches) {
+		
+			$time = microtime(true);
+			$embed = $this->discord->factory(Embed::class);
+			$embed->setAuthor("Melbourne Weather Radar", "https://beta.bom.gov.au/themes/custom/bom_theme/images/icons/favicon-32.png", "http://www.bom.gov.au/products/IDR023.loop.shtml")
+				->setImage("attachment://radar-{$time}.gif")
+				->setColor(getenv('COLOUR'));
+			$builder = MessageBuilder::new()
+				->addEmbed($embed)
+				->addFile("/Media/Radar/animated.gif", "radar-{$time}.gif");		
+			$message->channel->sendMessage($builder);
+		
+		}
+		
+	}
+	
+?>
