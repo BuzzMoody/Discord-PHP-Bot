fef41d2c2f5ce52ce3174242470047c128ea8535
Buzz Moody <buzz+github@buzz.id.au>
2025-11-07 20:12:53 +1100

    Update Stats.php

--- Commands/Stats.php
+++ Commands/Stats.php
@@ -5,8 +5,10 @@
 	use Psr\Http\Message\ResponseInterface;
 
 	class Stats extends AbstractCommand {
-		
-		const OSRS_SKILLS = ['Attack'=>'⚔️','Defence'=>'🛡️','Strength'=>'💪','Hitpoints'=>'❤️','Ranged'=>'🏹','Prayer'=>'🙏','Magic'=>'🔮','Cooking'=>'🍳','Woodcutting'=>'🌲','Fletching'=>'🪶','Fishing'=>'🎣','Firemaking'=>'🔥','Crafting'=>'💎','Smithing'=>'⚒️','Mining'=>'⛏️','Herblore'=>'🌿','Agility'=>'🤸‍♂️','Thieving'=>'🕵️‍♂️','Slayer'=>'☠️','Farming'=>'🌾','Runecraft'=>'🌀','Hunter'=>'🐾','Construction'=>'🧱','Overall'=>'🏆'];
+			
+		const OSRS_SKILLS = ['Attack','Hitpoints','Mining','Strength','Agility','Smithing','Defence','Herblore','Fishing','Ranged','Thieving','Cooking','Prayer','Crafting','Firemaking','Magic','Fletching','Woodcutting','Runecraft','Slayer','Farming','Construction','Hunter','Overall'];
+
+		const OSRS_SKILL_ICONS = ['Attack'=>'⚔️','Defence'=>'🛡️','Strength'=>'💪','Hitpoints'=>'❤️','Ranged'=>'🏹','Prayer'=>'🙏','Magic'=>'🔮','Cooking'=>'🍳','Woodcutting'=>'🌲','Fletching'=>'🪶','Fishing'=>'🎣','Firemaking'=>'🔥','Crafting'=>'💎','Smithing'=>'⚒️','Mining'=>'⛏️','Herblore'=>'🌿','Agility'=>'🤸‍♂️','Thieving'=>'🕵️‍♂️','Slayer'=>'☠️','Farming'=>'🌾','Runecraft'=>'🌀','Hunter'=>'🐾','Construction'=>'🧱','Overall'=>'🏆'];
 
 		public function getName(): string {
 			return 'F1';
@@ -23,7 +25,7 @@
 		public function execute($message, $args, $matches) {
 			
 			$player = $args;
-			if (!$player) { return $this->utils->simpleEmbed('OldSchool RuneScape - Hiscores', 'https://framerusercontent.com/images/uBhW5awsZ7NDMakiHaUgbgmOgg.png', 'Give me a player to look up!', $message, true, 'https://oldschool.runescape.com/'); }
+			if (!$player) { return $this->utils->simpleEmbed('OSRS - Hiscores', 'https://framerusercontent.com/images/uBhW5awsZ7NDMakiHaUgbgmOgg.png', 'Give me a player to look up!', $message, true, 'https://oldschool.runescape.com/'); }
 		
 			$http = new Browser();
 
@@ -32,11 +34,17 @@
 					$output = json_decode($response->getBody());
 					
 					$embed = $this->discord->factory(Embed::class);
-					$embed->setAuthor("OldSchool RuneScape - Hiscores - {$output->name}", 'https://framerusercontent.com/images/uBhW5awsZ7NDMakiHaUgbgmOgg.png', "https://secure.runescape.com/m=hiscore_oldschool/hiscorepersonal?user1={$output->name}")
+					$embed->setAuthor("OSRS - Hiscores - ".ucfirst($output->name), 'https://framerusercontent.com/images/uBhW5awsZ7NDMakiHaUgbgmOgg.png', "https://secure.runescape.com/m=hiscore_oldschool/hiscorepersonal?user1={$output->name}")
 						->setColor(getenv('COLOUR'));
-		
-					foreach ($output->skills as $skill) {
-						$embed->addFieldValues(self::OSRS_SKILLS[$skill->name], $skill->level, true);
+						
+					$skillsByName = [];
+					foreach ($output->skill as $skill) {
+						$skillsByName[$skill->name] = $skill;
+					}
+					print_r($skillsByName);
+					
+					foreach (self::OSRS_SKILLS as $name) {
+						$embed->addFieldValues(self::OSRS_SKILL_ICONS[name], $skillsByName[$name]->level, true);
 					}
 
 					$message->channel->sendEmbed($embed);
