f93f9c3d2b5c5d4ba466b206335bbd52446134f4
Buzz <buzz+github@buzz.id.au>
2025-11-24 21:08:16 +1100

    Update Stats.php

--- Commands/Stats.php
+++ Commands/Stats.php
@@ -41,8 +41,8 @@
 					}
 					foreach (self::OSRS_SKILLS as $name) {
 						$skillsByName[$name]->level = ($skillsByName[$name]->level <= 0) ? "NA" : $skillsByName[$name]->level;
-						$levels .= self::OSRS_SKILL_ICONS[$name].' '.str_pad($skillsByName[$name]->level, 2).'	-	';
-						if ($name == 'Overall' && $skillsByName[$name]->level != "NA") { $levels .= "\n\n📊 ".number_format($skillsByName[$name]->xp)."\n\n🏅 ".number_format($skillsByName[$name]->rank); }
+						$levels .= self::OSRS_SKILL_ICONS[$name].' '.str_pad($skillsByName[$name]->level, 2).' ```	test	```';
+						if ($name == 'Overall' && $skillsByName[$name]->level != "NA") { $levels .= "(".number_format($skillsByName[$name]->xp)." xp)\n\n🏅 ".number_format($skillsByName[$name]->rank); }
 						if (($x + 1) % 3 === 0) { $levels .= "\n\n"; }
 						$x++;
 					}
