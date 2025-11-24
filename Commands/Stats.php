8791d3abfd1cca43836f3f4af960bcfe1913fce3
Buzz <buzz+github@buzz.id.au>
2025-11-24 22:07:46 +1100

    Update Stats.php

--- Commands/Stats.php
+++ Commands/Stats.php
@@ -43,7 +43,7 @@
 						// if (in_array($name, ['Overall', 'Clues', 'Collections'])) { continue; }
 						
 						$skillsByName[$name]->level = ($skillsByName[$name]->level <= 0) ? "NA" : $skillsByName[$name]->level;
-						$levels .= self::OSRS_SKILL_ICONS[$name].'  ‎ ‎ '.str_pad($skillsByName[$name]->level, 2);
+						$levels .= self::OSRS_SKILL_ICONS[$name].'  '.str_pad($skillsByName[$name]->level, 2);
 						if (($x + 1) % 3 === 0) { $levels .= "\n\n"; }
 						else { $levels .= '‎ ‎ ‎ ‎ ‎  ‎ ‎ ‎  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ '; }
 						$x++;
